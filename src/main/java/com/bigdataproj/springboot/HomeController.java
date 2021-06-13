4d0d08d7bb36951529f0247b1a9f52831afea8a2
kiran <kiranrajbaburaj1992@gmail.com>
2021-06-13 10:17:07 +0530

    Configuration Changes

--- src/main/java/com/bigdataproj/springboot/HomeController.java
+++ src/main/java/com/bigdataproj/springboot/HomeController.java
@@ -34,9 +34,29 @@ public class HomeController {
 		Configuration conf = HBaseConfiguration.create();
 		System.out.println("--------------+++++++++++\n");
 		
-		conf.set("hbase.zookeeper.quorum", "quickstart.cloudera");
-		conf.set("hbase.zookeeper.property.clientPort", "2181");
 
+		//conf.set("hbase.master", "192.168.43.183:60001");
+//		conf.set("hbase.master.port", "60000");
+//		conf.set("hbase.zookeeper.quorum", "quickstart.cloudera");
+//		conf.set("hbase.zookeeper.property.clientPort", "2181");
+//		conf.set("hbase.rootdir","hdfs://quickstart.cloudera:8020/hbase");
+//		conf.set("hbase.client.retries.number", "4");
+//		conf.set("hbase.rpc.timeout", "30000");
+//		conf.set("hbase.security.authentication", "simple");
+//        conf.set("zookeeper.znode.parent", "/hbase");
+//		conf.set("fs.defaultFS", "hdfs://quickstart.cloudera:8020");
+		
+		
+		conf.set("hbase.master.port", "60001");
+		conf.set("hbase.zookeeper.quorum", "hdoop-VirtualBox");
+		conf.set("hbase.zookeeper.property.clientPort", "2181");
+		conf.set("hbase.rootdir","hdfs://hdoop-VirtualBox:9000/hbase");
+		conf.set("hbase.client.retries.number", "4");
+		conf.set("hbase.rpc.timeout", "30000");
+		conf.set("hbase.security.authentication", "simple");
+        conf.set("zookeeper.znode.parent", "/hbase");
+		conf.set("fs.defaultFS", "hdfs://hdoop-VirtualBox:9000");
+		
 		String result = "";
 		List<String> list = new ArrayList<String>();
 		try {
@@ -45,65 +65,130 @@ public class HomeController {
 		Scan scan1 = new Scan();
 		ResultScanner scanner1 = table.getScanner(scan1);
 		
-		for(Result res : scanner1) {
-			list.add(res.toString());
-			System.out.println(Bytes.toString(res.getRow()));
-			System.out.println(Bytes.toString(res.getValue("personal".getBytes(), "name".getBytes())));
-			System.out.println(Bytes.toString(res.getValue("personal".getBytes(),"salary".getBytes())));
-
-		}
+//		for(Result res : scanner1) {
+//			list.add(res.toString());
+//			System.out.println(Bytes.toString(res.getRow()));
+//			System.out.println(Bytes.toString(res.getValue("personal".getBytes(), "name".getBytes())));
+//			System.out.println(Bytes.toString(res.getValue("personal".getBytes(),"salary".getBytes())));
+//
+//		}
 		scanner1.close();
-		
-		
-		
-
-		// Put Object for update also
-		
-		Put put = new Put("7".getBytes());
-
-		put.addColumn("personal".getBytes(), "name".getBytes(), "value1".getBytes());
-		put.addColumn("personal".getBytes(), "salary".getBytes(), "value2".getBytes());
-		
-		table.put(put);
-		
-		
-		// Get Object
-		
+//		
+//		
+//		
+//
+//		// Put Object for update also
+//		
+//		Put put = new Put("7".getBytes());
+//
+//		put.addColumn("personal".getBytes(), "name".getBytes(), "value1".getBytes());
+//		put.addColumn("personal".getBytes(), "salary".getBytes(), "value2".getBytes());
+//		
+//		table.put(put);
+//		
+//		
+//		// Get Object
+//		
 		Get get = new Get("3".getBytes());
 		Result getResult = table.get(get);
 		System.out.println(getResult);
-		System.out.println(Bytes.toString(getResult.getValue("personal".getBytes(), "name".getBytes())));
-		System.out.println(Bytes.toString(getResult.getValue("personal".getBytes(),"salary".getBytes())));
-		
-		
-		//Delete Object
-		Delete del = new Delete("1".getBytes());
-		table.delete(del);
-		
-		list = new ArrayList<String>();
-		scanner1 = table.getScanner(scan1);
-		for(Result res : scanner1) {
-			list.add(res.toString());
-			System.out.println(Bytes.toString(res.getRow()));
-			System.out.println(Bytes.toString(res.getValue("personal".getBytes(), "name".getBytes())));
-			System.out.println(Bytes.toString(res.getValue("personal".getBytes(),"salary".getBytes())));
-
-		}
-		
-		scanner1.close();
-		
-		
+//		System.out.println(Bytes.toString(getResult.getValue("personal".getBytes(), "name".getBytes())));
+//		System.out.println(Bytes.toString(getResult.getValue("personal".getBytes(),"salary".getBytes())));
+//		
+//		
+//		//Delete Object
+//		Delete del = new Delete("1".getBytes());
+//		table.delete(del);
+//		
+//		list = new ArrayList<String>();
+//		scanner1 = table.getScanner(scan1);
+//		for(Result res : scanner1) {
+//			list.add(res.toString());
+//			System.out.println(Bytes.toString(res.getRow()));
+//			System.out.println(Bytes.toString(res.getValue("personal".getBytes(), "name".getBytes())));
+//			System.out.println(Bytes.toString(res.getValue("personal".getBytes(),"salary".getBytes())));
+//
+//		}
+//		
+//		scanner1.close();
+//		
+//		
 		}catch(Exception e) {
-			
+			System.out.print(e);
 		}
 		
 		
 		
 		
 		ModelAndView map = new ModelAndView("home.jsp");
-	    map.addObject("lists", list);
+	    //map.addObject("lists", list);
 	    return map;
 		
 	}
 
 }
+
+
+
+
+/*
+ * 
+ * hbase-site-env.xml
+ *  
+<configuration>
+<property>
+<name>hbase.rootdir</name>
+<value>hdfs://hdoop-VirtualBox:9000/hbase</value>
+</property>
+
+<property>
+<name>hbase.master.port</name>
+<value>60001</value>
+</property>
+
+<property>
+<name>hbase.cluster.distributed</name>
+<value>true</value>
+</property>
+
+<property>
+<name>hbase.zookeeper.property.dataDir</name>
+<value>/home/hdoop/zookeeper</value>
+</property>
+
+<property>
+<name>hbase.zookeeper.property.maxClientCnxns</name>
+<value>35</value>
+</property>
+
+<property>
+<name>hbase.zookeeper.quorum</name>
+<value>hdoop-VirtualBox</value>
+</property>
+
+<property>
+<name>hbase.zookeeper.property.clientPort</name>
+<value>2181</value>
+</property>
+
+<property>
+<name>hbase.unsafe.stream.capability.enforce</name>
+<value>false</value>
+</property>
+
+<property>
+<name>hbase.regionserver.ipc.address</name>
+<value>0.0.0.0</value>
+</property>
+
+<property>
+<name>hbase.master.ipc.address</name>
+<value>0.0.0.0</value>
+</property>
+</configuration>
+ 
+ 
+ * 
+ * 
+ * 
+ */
